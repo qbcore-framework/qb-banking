e883f91d822d1871103e0c87d9b860bd711aac34
QBCore Framework <81791099+qbcore-framework@users.noreply.github.com>
2021-06-09 21:31:46 -0500

    comma

--- server/wrappers/useraccounts.lua
+++ server/wrappers/useraccounts.lua
@@ -356,7 +356,7 @@ function generateSavings(cid)
                 local success = self.saveAccount()
 
                 local time = os.date("%Y-%m-%d %H:%M:%S")
-                QBCore.Functions.ExecuteSql(false, "INSERT INTO `bank_statements` (`citizenid`,  `account`  `deposited`, `withdraw`, `balance`, `date`, `type`) VALUES ('" .. self.cid .. "', 'Saving', 0,'" .. amt .. "', '" .. self.balance .. "', '" .. time .. "', '" .. text .. "')")
+                QBCore.Functions.ExecuteSql(false, "INSERT INTO `bank_statements` (`citizenid`,  `account`,  `deposited`, `withdraw`, `balance`, `date`, `type`) VALUES ('" .. self.cid .. "', 'Saving', 0,'" .. amt .. "', '" .. self.balance .. "', '" .. time .. "', '" .. text .. "')")
                 local statementTable = {['withdraw'] = amt, ['deposited'] = nil, ['type'] = text,  ['date'] = time, ['balance'] = self.balance, ['account'] = "Savings", ['record_id'] = statementUpdate, ['character_id'] = self.cid }
                 table.insert(self.bankStatement, statementTable)
                 return true
