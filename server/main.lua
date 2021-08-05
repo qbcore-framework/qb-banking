b9e52fdc9493cecf2aabea8989cbc10d4f1820eb
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-05 11:14:22 +0300

    add give cash command

--- server/main.lua
+++ server/main.lua
@@ -408,3 +408,47 @@ AddEventHandler('qb-banking:createSavingsAccount', function()
     TriggerClientEvent('qb-banking:successAlert', src, 'You have successfully opened a savings account.')
     TriggerEvent('qb-log:server:CreateLog', 'banking', 'Banking', "Created new saving account", src)
 end)
+
+
+QBCore.Commands.Add('givecash', 'Give cash to player.', {{name = 'id', help = 'Player ID'}, {name = 'amount', help = 'Amount'}}, true, function(source, args)
+    local src = source
+	local id = tonumber(args[1])
+	local amount = math.ceil(tonumber(args[2]))
+    
+	if id and amount then
+		local xPlayer = QBCore.Functions.GetPlayer(src)
+		local xReciv = QBCore.Functions.GetPlayer(id)
+		
+		if xReciv and xPlayer then
+			if not xPlayer.PlayerData.metadata["isdead"] then
+				local distance = xPlayer.PlayerData.metadata["inlaststand"] and 3.0 or 10.0
+				if #(GetEntityCoords(GetPlayerPed(src)) - GetEntityCoords(GetPlayerPed(id))) < distance then
+					if xPlayer.Functions.RemoveMoney('cash', amount) then
+						if xReciv.Functions.AddMoney('cash', amount) then
+							TriggerClientEvent('QBCore:Notify', src, "Success fully gave to ID " .. tostring(id) .. ' ' .. tostring(amount) .. '$.', "success")
+							TriggerClientEvent('QBCore:Notify', id, "Success recived gave " .. tostring(amount) .. '$ from ID ' .. tostring(src), "success")
+							TriggerClientEvent("payanimation", src)
+						else
+							TriggerClientEvent('QBCore:Notify', src, "Could not give item to the given id.", "error")
+						end
+					else
+						TriggerClientEvent('QBCore:Notify', src, "You don\'t have this amount.", "error")
+					end
+				else
+					TriggerClientEvent('QBCore:Notify', src, "You are too far away lmfao.", "error")
+				end
+			else
+				TriggerClientEvent('QBCore:Notify', src, "You are dead LOL.", "error")
+			end
+		else
+			TriggerClientEvent('QBCore:Notify', src, "Wrong ID.", "error")
+		end
+	else
+		TriggerClientEvent('QBCore:Notify', src, "Usage /givecash [ID] [AMOUNT]", "error")
+	end
+end)
+
+RegisterNetEvent("payanimation")
+AddEventHandler("payanimation", function()
+TriggerEvent('animations:client:EmoteCommandStart', {"id"})
+end)
