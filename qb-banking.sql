f796bb0b0f20436be6a79b9649a996df46262150
Menkzy <iammenkz@gmail.com>
2021-10-23 13:39:36 +1100

    fix(sql): buisness > business
    qb-sql has "buisness" and the resource calls for "business"

--- qb-banking.sql
+++ qb-banking.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS `bank_accounts` (
   `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(250) DEFAULT NULL,
-  `buisness` varchar(50) DEFAULT NULL,
-  `buisnessid` int(11) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
   `gangid` varchar(50) DEFAULT NULL,
   `amount` bigint(255) NOT NULL DEFAULT 0,
-  `account_type` enum('Current','Savings','Buisness','Gang') NOT NULL DEFAULT 'Current',
+  `account_type` enum('Current','Savings','Business','Gang') NOT NULL DEFAULT 'Current',
   PRIMARY KEY (`record_id`),
   UNIQUE KEY `citizenid` (`citizenid`),
-  KEY `buisness` (`buisness`),
-  KEY `buisnessid` (`buisnessid`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
   KEY `gangid` (`gangid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS `bank_statements` (
   `record_id` bigint(255) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) DEFAULT NULL,
   `account` varchar(50) DEFAULT NULL,
-  `buisness` varchar(50) DEFAULT NULL,
-  `buisnessid` int(11) DEFAULT NULL,
+  `business` varchar(50) DEFAULT NULL,
+  `businessid` int(11) DEFAULT NULL,
   `gangid` varchar(50) DEFAULT NULL,
   `deposited` int(11) DEFAULT NULL,
   `withdraw` int(11) DEFAULT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `bank_statements` (
   `date` varchar(50) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`record_id`),
-  KEY `buisness` (`buisness`),
-  KEY `buisnessid` (`buisnessid`),
+  KEY `business` (`business`),
+  KEY `businessid` (`businessid`),
   KEY `gangid` (`gangid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
