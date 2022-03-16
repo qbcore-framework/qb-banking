2a14cddab2159b43a1aabdb48f02730a12937869
Keaton <keatonwyd@gmail.com>
2022-03-15 20:31:21 -0400

    Added English Locale

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,11 @@ game 'gta5'
 description 'QB-Banking'
 version '1.0.0'
 
-shared_script 'config/config.lua'
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'config/config.lua'
+}
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
