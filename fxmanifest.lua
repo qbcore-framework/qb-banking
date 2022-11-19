06c10a13708e717d3ad89eaab51d6777f0e258d6
Charlie <dev@charlie.fyi>
2022-11-19 12:40:21 +0000

    Bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Banking'
-version '1.0.0'
+version '1.2.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
@@ -10,7 +10,6 @@ shared_scripts {
     'locales/*.lua',
     'config/config.lua'
 }
-
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
